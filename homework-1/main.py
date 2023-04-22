9de947776860824b327593bd534c7f94b652f3a0
Alexey Grazhdankin <alex.alexxx56@gmail.com>
2023-04-22 21:24:13 +0300

    added class Node and class Stack with method push()

--- homework-1/main.py
+++ homework-1/main.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     print(n1)  # <__main__.Node object at 0x0000022803036050>
     print(n2.next_node)  # <__main__.Node object at 0x0000022803036050>
 
-    stack = Stack()
-    stack.push('data1')
-    stack.push('data2')
-    stack.push('data3')
-    print(stack.top.data)  # data3
-    print(stack.top.next_node.data)  # data2
-    print(stack.top.next_node.next_node.data)  # data1
-    print(stack.top.next_node.next_node.next_node)  # None
-    print(stack.top.next_node.next_node.next_node.data)  # AttributeError: 'NoneType' object has no attribute 'data'
+    # stack = Stack()
+    # stack.push('data1')
+    # stack.push('data2')
+    # stack.push('data3')
+    # print(stack.top.data)  # data3
+    # print(stack.top.next_node.data)  # data2
+    # print(stack.top.next_node.next_node.data)  # data1
+    # print(stack.top.next_node.next_node.next_node)  # None
+    # print(stack.top.next_node.next_node.next_node.data)  # AttributeError: 'NoneType' object has no attribute 'data'
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src.stack import Stack
+
+
+class TestStack(unittest.TestCase):
+    def test_push(self):
+        test_stack = Stack()
+        test_stack.push("One")
+        self.assertEqual(test_stack.stack_list, ["One"])
+        test_stack.push("Two")
+        self.assertEqual(test_stack.stack_list, ["Two", "One"])
+
+if __name__ == '__main__':
+    unittest.main()
